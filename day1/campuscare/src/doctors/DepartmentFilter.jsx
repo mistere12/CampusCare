e39b7f801c3e91967f32868f99c88fabe5e46b06
Amele20002 <ameleworkabate3@gmail.com>
2026-09-25 01:47:01 -0500

    feat: add department filter

---
+++ day1/campuscare/src/doctors/DepartmentFilter.jsx
@@ -0,0 +1,37 @@
+function DepartmentFilter({ department, setDepartment }) {
+  return (
+    <div>
+      <label htmlFor="department">
+        Filter by department:
+      </label>
+
+      <select
+        id="department"
+        value={department}
+        onChange={(event) =>
+          setDepartment(event.target.value)
+        }
+      >
+        <option value="All">All Departments</option>
+
+        <option value="Cardiology">
+          Cardiology
+        </option>
+
+        <option value="Pediatrics">
+          Pediatrics
+        </option>
+
+        <option value="Dermatology">
+          Dermatology
+        </option>
+
+        <option value="General Medicine">
+          General Medicine
+        </option>
+      </select>
+    </div>
+  );
+}
+
+export default DepartmentFilter;
